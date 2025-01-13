500f9a66acdabd1d07c3cea0db268a1d8a5f96b4
Ayush kumar <kumarayush042004@gmail.com>
2025-01-13 21:46:09 +0530

    css

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ function LogoutBtn() {
     }
 
     return (
-        <button className="inline-block px-6 duration-200 hover:bg-blue-100 rounded-full"
+        <button className="px-6 py-2  text-white duration-200 hover:bg-blue-100 hover:text-black rounded-full shadow-md"
         onClick={logoutHandler}>Logout</button>
     )
 }
