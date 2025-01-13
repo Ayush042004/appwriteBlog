4ebbd6a4850f7ee5bc9a144168aa92eeec56efa3
Ayush kumar <kumarayush042004@gmail.com>
2025-01-13 19:57:54 +0530

    css changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React  from 'react';
 import { Container, Logo , LogoutBtn } from '../index';
 import {Link} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+
 
 function Header() {
     {/* status access kare ha hum use slector redux se  */}
@@ -39,21 +41,22 @@ function Header() {
         },
     ]
     return (
-        <header className='py-3 shadow bg-gray-500'>
+        <header className='shadow bg-gray-500 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 text-white'>
             <Container>
-                <nav className='flex'>
+                <nav className='flex items-center '>
                     <div className='mr-4'> 
                         <Link to='/'>
                         <Logo width='70px'/>
                         </Link>
                     </div>
+                  
 
-                    <ul className='flex ml-auto'>
+                    <ul className='flex ml-auto items-center  space-x-4'>
                         {navItems.map((item)=> item.active ? (
                             <li key={item.name}>
                               <button
                 onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                className=' px-6 py-2  text-white duration-200 hover:bg-blue-100 hover:text-black rounded-full shadow-md'
                 >{item.name}</button>
                             </li>
                         ): null
--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // this comp is for whole the actual project lies under it thats why we passed chidren as prop and refrence
 function Container ({children}) {
-    return <div className="w-full max-w-7xl mx-auto px-4">
+    return <div className="w-full max-w-7xl mx-auto px-4  py-6  sm:px-6 lg:px-8">
         {children}
     </div>;  
 }
