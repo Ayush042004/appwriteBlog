11cd675ded794d5a90624abd8d355329e6bb0247
Ayush kumar <kumarayush042004@gmail.com>
2025-01-13 21:26:21 +0530

    css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,10 @@ import Logo from '../Logo'
 
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
-            <div className="relative z-10 mx-auto max-w-7xl px-4">
-                <div className="-m-6 flex flex-wrap">
+    <section className="relative overflow-hidden py-14 bg-gray-400  border border-t-2 border-t-black">
+            <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="-m-6 flex flex-wrap justify-between">
+
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                         <div className="flex h-full flex-col justify-between">
                             <div className="mb-4 inline-flex items-center">
@@ -14,14 +15,16 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-gray-600">
-                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
+                                    &copy; {new Date().getFullYear()} All Rights Reserved
                                 </p>
                             </div>
                         </div>
                     </div>
+
+
                     <div className="w-full p-6 md:w-1/2 lg:w-2/12">
                         <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
+                            <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500 ">
                                 Company
                             </h3>
                             <ul>
