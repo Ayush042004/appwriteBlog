2723a532090b837fcc398900fb502787e9a2d3d7
Ayush kumar <kumarayush042004@gmail.com>
2025-01-16 14:54:49 +0530

    css changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,12 +8,12 @@ function PostCard({$id, title, featuredImage}) {
     //console.log("Featured Image:", featuredImage);
     return (
         <Link to={`/post/${$id}`}>
-            <div className="w-full bg-gray-100 rounded-xl p-4">
+            <div className="w-full bg-gray-100 rounded-lg shadow-lg p-4 transition-transform transform hover:scale-105 hover:shasow-2xl hover:bg-slate-500">
                 <div className="w-full justify-center mb-4">
                    <img src={preview} alt={title}
-                   className="rounded-xl"/>
+                   className="rounded-lg object-cover w-full h-40  sm:h-48 md:h-56 lg:h-64"/>
                 </div>
-                <h2 className="text-xl font-bold">{title}</h2>
+                <h2 className="text-base sm:text-lg md:text-xl font-bold text-gray-800 truncate text-center">{title}</h2>
             </div>
         </Link>
     )
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -12,10 +12,10 @@ export default function Post() {
 
     const userData = useSelector((state) => state.auth.userData);
 
-    const isAuthor = post && userData ? post.userId === userData.$id : false;
+    const isAuthor = post && userData ? post.userId === userData.$id : false;//agr post ha or user data ha toh check kro k post ka user id user data ka id ha ya nhi
 
     useEffect(() => {
-        if (slug) {
+        if (slug) {   //agr slug ha toh post ko get kro post ha toh set kro nhi toh home page pe navigate kro
             appwriteService.getPost(slug).then((post) => {
                 if (post) setPost(post);
                 else navigate("/");
@@ -23,7 +23,7 @@ export default function Post() {
         } else navigate("/");
     }, [slug, navigate]);
 
-    const deletePost = () => {
+    const deletePost = () => {  //agr post ha toh delete krdo or uska featured image bhi delete krdo
         appwriteService.deletePost(post.$id).then((status) => {
             if (status) {
                 appwriteService.deleteFile(post.featuredImage);
@@ -35,15 +35,15 @@ export default function Post() {
     return post ? (
         <div className="py-8">
             <Container>
-                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+                <div className="w-full flex  flex-col md:flex-row justify-center mb-4 relative border rounded-xl p-2 shadow-lg">
                     <img
                         src={appwriteService.getFilePreview(post.featuredImage)}
                         alt={post.title}
-                        className="rounded-xl"
+                        className="rounded-xl w-full md:w-1/2" 
                     />
 
                     {isAuthor && (
-                        <div className="absolute right-6 top-6">
+                        <div className="absolute right-6 top-6 flex space-x-3">
                             <Link to={`/edit-post/${post.$id}`}>
                                 <Button bgColor="bg-green-500" className="mr-3">
                                     Edit
@@ -55,7 +55,7 @@ export default function Post() {
                         </div>
                     )}
                 </div>
-                <div className="w-full mb-6">
+                <div className="w-full mb-6 px-4 md:px-0">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
                 </div>
                 <div className="browser-css">
